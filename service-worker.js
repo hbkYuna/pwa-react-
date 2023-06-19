64d8381fb1c6225fc407e13b0ead4726952be79b
HbkYuna <strawhatnxahx@gmail.com>
2023-06-19 12:23:04 +0200

    Service worker

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+// Service Worker version
+const version = '1.0.0';
+
+// Cache name
+const cacheName = `your-app-cache-${version}`;
+
+const filesToCache = [
+  './', 
+  'index.html',
+  'HomeScreen.js',
+  'App.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== cacheName)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
+
+
+
